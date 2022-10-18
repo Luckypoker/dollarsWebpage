7d263bd5387c957db76137d93e2bf052b01416d2
Tyler <geniusatwork543@gmail.com>
2022-10-18 15:53:14 -0400

    began variable declarations

---
+++ content/java.js
@@ -0,0 +1,6 @@
+//declarations
+const container = document.querySelector('#container')
+const main = document.querySelector('.main')
+const divpopup = document.querySelector('.divpopup')
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
